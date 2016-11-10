d2e022ea99d01bb557872f763e2190301a03402f
EnriqueTor <etorrendell@gmail.com>
2016-11-10 17:59:30 -0500

    Complete

--- SlapChat/AddMessageViewController.swift
+++ SlapChat/AddMessageViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 import CoreData
 
 class AddMessageViewController: UIViewController {
-
+    
     @IBOutlet weak var addMessageTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func saveMessageButtonTapped(_ sender: AnyObject) {
         let store = DataStore.sharedInstance
         let context = store.persistentContainer.viewContext
@@ -31,5 +31,39 @@ class AddMessageViewController: UIViewController {
     @IBAction func cancelButtonTapped(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
+}
+
+class AddRecipientViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var twitterField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        
+        let store = DataStore.sharedInstance
+        let context = store.persistentContainer.viewContext
+        let newRecipient = NSEntityDescription.insertNewObject(forEntityName: "Recipient", into: context) as! Recipient
+        newRecipient.name = textField.text
+        newRecipient.email = emailField.text
+        newRecipient.phoneNumber = phoneField.text
+        newRecipient.twitterHandle = twitterField.text
+        store.saveContext()
+        store.recipients.append(newRecipient)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    
 }
 
--- SlapChat/RecipientTableViewController.swift
+++ SlapChat/RecipientTableViewController.swift
@@ -1,55 +1,58 @@
 //
-//  TableViewController.swift
+//  RecipientTableViewController.swift
 //  SlapChat
 //
-//  Created by Ian Rahman on 7/16/16.
+//  Created by Enrique Torrendell on 11/10/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class TableViewController: UITableViewController {
+class RecipientTableViewController: UITableViewController {
     
     var store = DataStore.sharedInstance
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         store.fetchData()
+        self.tableView.reloadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
         super.viewWillAppear(true)
-        
         store.fetchData()
         tableView.reloadData()
     }
 
-    // MARK: - Table view data source
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return store.messages.count
+        return store.recipients.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recipientCell", for: indexPath)
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
-
-        let eachMessage = store.messages[indexPath.row]
-
-        if let unwrappedMessage = eachMessage.content {
-        
-        cell.textLabel?.text = unwrappedMessage
-
+        if let unwrappedName = store.recipients[indexPath.row].name {
+            
+        cell.textLabel?.text = unwrappedName
+ 
         }
         
         return cell
     }
-    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueMessage" {
+            
+            
+            
+        }
+        
+    }
+
 }
